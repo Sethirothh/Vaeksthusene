b55982391ccae6a98c90582650ad966e783726ae
Jesper Gyldenløve <jespergyldenlve@gmail.com>
2019-03-31 16:19:20 +0200

    Styling af forside+spørgsmål

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -14,24 +14,35 @@ var afrika = [
   },
   {
     img: 'avocado-tree',
-    question: 'hej allesammen',
-    svar1: 'Jamen dav',
-    svar2: 'Nådada',
-    svar3: 'Jaja',
-    svar4: 'Nejnej'
+    question: 'Hvordan påvirker klimaforandringerne hungersnøden?',
+    svar1: 'Positivt',
+    svar2: 'Negativt',
+    svar3: 'Ingen Påvirkning',
+    svar4: 'Ubetydelig Påvirkning',
+    uddybelse: '',
+    next: 'second',
+    correct: 'two'
   },
   {
-    question: '',
-    svar1: '',
-    svar2: '',
-    svar3: '',
-    svar4: ''
+    img: 'avocado-tree',
+    question: 'Hvad er hungersnød?',
+    svar1: 'At folk mangler mad og er underernæret',
+    svar2: 'At de ikke har råd til at spise på restaurant',
+    svar3: 'At der kun bliver serveret Grøntsager og Ris',
+    svar4: 'At man er veganer og ikke har råd til grøntsager',
+    uddybelse: '',
+    next: 'third',
+    correct: 'one'
   },
   {
-    question: '',
-    svar1: '',
-    svar2: '',
-    svar3: '',
-    svar4: ''
+    img: 'avocado-tree',
+    question: 'Hvad er hungersnød?',
+    svar1: 'At folk mangler mad og er underernæret',
+    svar2: 'At de ikke har råd til at spise på restaurant',
+    svar3: 'At der kun bliver serveret Grøntsager og Ris',
+    svar4: 'At man er veganer og ikke har råd til grøntsager',
+    uddybelse: '',
+    next: 'fourth',
+    correct: 'one'
   }
 ]
