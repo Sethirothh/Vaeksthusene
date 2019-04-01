3f03ef6c81ace45c1ff773fc0384fe964e8c4801
Asger Nielsen <asgernielsen91@gmail.com>
2019-04-01 13:51:46 +0200

    stasherror fixed

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -40,14 +40,37 @@ var afrika = [
     correct: 'three'
   },
   {
-    img: 'avocado-tree',
-    question: 'Hvad er hungersnød?',
-    svar1: 'At folk mangler mad og er underernæret',
-    svar2: 'At de ikke har råd til at spise på restaurant',
-    svar3: 'At der kun bliver serveret Grøntsager og Ris',
-    svar4: 'At man er veganer og ikke har råd til grøntsager',
+    img: 'Pomelo',
+    question: 'Hvilken del af verdenen lider mest af hungersnød?',
+    svar1: 'Afrika',
+    svar2: 'Asien',
+    svar3: 'Nordamerika',
+    svar4: 'Sydamerika',
     uddybelse: '',
     next: 'fourth',
-    correct: 'one'
+    correct: 'two'
+  },
+  {
+  img: 'sortpeber',
+  question: 'Hvor stor en procentdel af børn under 5 år dør årligt på grund af sult?',
+  svar1: '10%',
+  svar2: '25%',
+  svar3: '35%',
+  svar4: '45%',
+  uddybelse: '',
+  next: 'fourth',
+  correct: 'four'
+  },
+  {
+    img: 'kokos',
+    question: 'Hvor stor en procentdel af børn under 5 år dør årligt på grund af sult?',
+    svar1: '10%',
+    svar2: '25%',
+    svar3: '35%',
+    svar4: '45%',
+    uddybelse: '',
+    next: 'fourth',
+    correct: 'four'
   }
+
 ]
